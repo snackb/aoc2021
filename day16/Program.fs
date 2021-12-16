f652bb6b75bcabd9bcb7931c6e1744254dbce3ab
Jack Blanchard <jack.blanchard88@gmail.com>
2021-12-17 08:43:13 +1100

    start of day16

---
+++ day16/Program.fs
@@ -0,0 +1,19 @@
+﻿open System.IO
+
+type PacketType<'a> = Literal of int | Operator of 'a seq
+type Packet = { Version: int; Type: PacketType<Packet>}
+
+let line = File.ReadLines("input") |> Seq.head
+
+let hexToBinary (input: char) = 
+    string input
+    |> (fun x -> System.Convert.ToInt32(x, 16)
+
+let parseList =
+    List.toArray<char> >> System.String
+    >> (fun x -> System.Convert.ToInt32(x, 2))
+
+let recParsePacket (input: char list): Packet =
+    let version = List.take 3 input |> parseList
+    let type = List.skip 3 input |> List.take 3
+
