d17c2327b1e7e8eb89a8fb8b17edad2b8552f3b3
Jack Blanchard <jblanchard@deloitte.com.au>
2021-12-17 20:28:01 +1100

    start day17

---
+++ day17/Program.fs
@@ -0,0 +1,36 @@
+﻿open System.IO
+open System
+
+// let line = File.ReadLines("input") |> Seq.head
+
+type Coord = int * int
+
+let trajectory x y: Coord seq = 
+    let mutable (xc, yc) = (0, 0)
+    let mutable xvel = x
+    let mutable yvel = y
+
+    seq {
+        while true do
+            xc <- xc + xvel
+            yc <- yc + yvel
+            xvel <- xvel - (if xvel = 0 then 0 else xvel / abs(xvel))
+            yvel <- yvel - 1
+            yield (xc, yc) 
+    }
+
+printfn "%A" ((trajectory 2 2) |> Seq.take 5)
+
+let between a b i = i < (max a b) && i > (min a b)
+
+let maxy minxc maxxc minyc maxyc =
+    seq {1..maxxc} 
+    |> Seq.allPairs (seq {0..1000})
+    |> Seq.map (fun (x, y) -> trajectory x y)
+    |> Seq.map (fun traj -> Seq.takeWhile (fun (x, y) -> x < maxxc + 1 && y > (min maxyc minyc) + 1) traj)
+    |> Seq.filter (fun traj -> Seq.exists (fun (x, y) -> (between minxc maxxc x) && (between maxyc minyc y)) traj)
+    |> Seq.map (fun traj -> Seq.maxBy (fun (_, y) ->  y) traj)
+    |> Seq.map (fun (x, y) -> y)
+    |> Seq.max
+
+printfn "%A" (maxy 20 30 -10 -5)
